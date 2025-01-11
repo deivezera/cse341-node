df2111e29442307bbf51e0c31f193d09cc570ab5
deivezera <dzabroski@gmail.com>
2025-01-11 17:33:39 -0300

    connection to mongodb and contactController

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
-app.get('/', (req,res) => {
-  res.send('Hello');
-})
+app.use('/', require('./routes'))
 
 const port = 3000;
-
-app.listen(process.env.port || port);
 console.log("Web Server is listening at port " + (process.env.port || port))
+
+mongodb.initDb((err) => {
+  if(err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => console.log(`Database is listening and node Running on port ${port}`))
+  }
+})
